922b42b17c986aa6d6264f78feca367762b9a1c0
pierrelouys <75588890+pierrelouys@users.noreply.github.com>
2020-12-08 10:01:11 +0100

    more rescued bats

---
+++ Inputmaps/Keen4.bat
@@ -0,0 +1,15 @@
+@echo OFF
+
+INPUTMAP up up
+INPUTMAP right right
+INPUTMAP down down
+INPUTMAP left left
+INPUTMAP triangle enter
+INPUTMAP cross lctrl
+INPUTMAP square space
+INPUTMAP circle lalt
+INPUTMAP start enter
+INPUTMAP select esc
+INPUTMAP rtrigger y
+INPUTMAP ltrigger f1
+INPUTMAP exec keen4e
