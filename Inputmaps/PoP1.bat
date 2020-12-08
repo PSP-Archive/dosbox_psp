85c52eee4a292469f6a2815624015e1b3f2d94af
pierrelouys <75588890+pierrelouys@users.noreply.github.com>
2020-12-08 10:01:11 +0100

    more rescued bats

---
+++ Inputmaps/PoP1.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+INPUTMAP up up
+INPUTMAP right right
+INPUTMAP down down
+INPUTMAP left left
+INPUTMAP triangle down
+INPUTMAP square lshift
+INPUTMAP cross up
+INPUTMAP circle circle
+INPUTMAP start enter
+INPUTMAP select esc
+INPUTMAP exec prince
---
+++ Inputmaps/PoP2.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+INPUTMAP up up
+INPUTMAP right right
+INPUTMAP down down
+INPUTMAP left left
+INPUTMAP triangle lalt
+INPUTMAP square lshift
+INPUTMAP cross up
+INPUTMAP triangle lalt
+INPUTMAP circle lctrl
+INPUTMAP start enter
+INPUTMAP select esc
+INPUTMAP rtrigger g
+INPUTMAP ltrigger l
+INPUTMAP exec prince
