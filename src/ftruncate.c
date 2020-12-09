5202db76c2272caf007ae12b18141ae15ec207cf
pierrelouys <75588890+pierrelouys@users.noreply.github.com>
2020-12-09 19:36:47 +0100

    Update ftruncate.c
    another Wayback Machine find from ps2dev.org

--- src/ftruncate.c
+++ src/ftruncate.c
@@ -1,6 +1,32 @@
 #include <stuff>
 #include "ftruncate.h"
 
-int ftruncate(int fd, off_t length){
-return 0;
-}
+int ftruncate(int fd, off_t length)
+{
+   int ret;
+   SceOff oldpos;
+   if (!__PSP_IS_FD_VALID(fd)) {
+      errno = EBADF;
+      return -1;
+   }
+
+   switch(__psp_descriptormap[fd]->type)
+   {
+      case __PSP_DESCRIPTOR_TYPE_FILE:
+         if (__psp_descriptormap[fd]->filename != NULL) {
+            if (!(__psp_descriptormap[fd]->flags & (O_WRONLY | O_RDWR)))
+               break;
+            return truncate(__psp_descriptormap[fd]->filename, length);
+            /* ANSI sez ftruncate doesn't move the file pointer */
+         }
+         break;
+      case __PSP_DESCRIPTOR_TYPE_TTY:
+      case __PSP_DESCRIPTOR_TYPE_PIPE:
+      case __PSP_DESCRIPTOR_TYPE_SOCKET:
+      default:
+         break;
+   }
+
+   errno = EINVAL;
+   return -1;
+} 
